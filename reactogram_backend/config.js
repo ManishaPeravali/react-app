97ce536da00e54c737bb085a88d7a0491b2164ba
ManishaPeravali <manisha121peravali@gmail.com>
2023-06-22 10:34:12 +0530

    first commit

--- reactogram_backend/config.js
+++ reactogram_backend/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  MONGOBD_URL: "mongodb://127.0.0.1:27017/reactogram",
+  MONGOBD_URL: "mongodb+srv://React:React@cluster0.xa2mkva.mongodb.net/firstdb?retryWrites=true&w=majority",
   JWT_SECRET: "fhgsefuilhgvsdfhvldgvigdfbkuvgdyukbyudglvsg",
 };
